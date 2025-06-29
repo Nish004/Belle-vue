86b521144032f38feafd412e321df71256fc363a
Nish004 <nisanthsmpupi@gmail.com>
2025-06-29 11:52:10 +0530

    dgsj

--- Backend/resort.sql
+++ Backend/resort.sql
@@ -34,7 +34,7 @@ CREATE TABLE `bookings` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,8 +43,35 @@ CREATE TABLE `bookings` (
 
 LOCK TABLES `bookings` WRITE;
 /*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
-INSERT INTO `bookings` VALUES (1,9,2,'2025-06-22','2025-06-25',450.00,'pending','2025-06-22 15:36:00'),(2,9,2,'2025-06-22','2025-06-24',300.00,'pending','2025-06-22 15:55:14'),(3,9,3,'2025-06-15','2025-06-23',1600.00,'pending','2025-06-22 18:27:56'),(4,9,4,'2025-06-22','2025-06-24',500.00,'pending','2025-06-22 18:29:04'),(5,9,1,'2025-06-24','2025-06-25',120.00,'pending','2025-06-22 18:32:51'),(6,9,1,'2025-06-23','2025-06-24',120.00,'pending','2025-06-22 18:41:29');
+INSERT INTO `bookings` VALUES (7,12,1,'2025-06-25','2025-06-26',120.00,'approved','2025-06-24 15:19:50'),(8,12,1,'2025-06-26','2025-06-27',120.00,'approved','2025-06-25 06:04:43');
 /*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `message` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+INSERT INTO `messages` VALUES (1,'nisanth sm','nisanthsmpupi@gmail.com','podaaaa','2025-06-28 19:57:56');
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -60,6 +87,7 @@ CREATE TABLE `rooms` (
   `type` varchar(50) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL,
+  `description` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -70,7 +98,7 @@ CREATE TABLE `rooms` (
 
 LOCK TABLES `rooms` WRITE;
 /*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
-INSERT INTO `rooms` VALUES (1,'Classic Room','classic','/assets/rooms/classic1.avif',120.00),(2,'Corner Room','corner','/assets/rooms/corner2.avif',150.00),(3,'Superior Room','superior','/assets/rooms/superior1.jfif',200.00),(4,'Terrace Room','terrace','/assets/rooms/terrace1.jfif',250.00),(5,'Lakeside Villa','luxury_villa','/assets/gallery5.jpg',450.00),(6,'Forest Suite','forest_suite','/assets/gallery2.jfif',350.00),(7,'Executive Room','executive_room','/assets/gallery3.jfif',275.00);
+INSERT INTO `rooms` VALUES (1,'Classic Room','classic','/uploads/classic1.avif',120.00,'awsome'),(2,'Corner Room','corner','/uploads/corner2.avif',150.00,NULL),(3,'Superior Room','superior','/uploads/superior1.jfif',200.00,'good feelgood '),(4,'Terrace Room','terrace','/uploads/terrace1.jfif',250.00,NULL);
 /*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +122,7 @@ CREATE TABLE `users` (
   `avatar` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,7 +131,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'test','test@example.com','$2b$10$rD71ZaxyI1si3GpOXiczkubC7rIhCyiIaKhJiZRDyf6XsO78X4YXm','user','2025-06-21 17:30:27',NULL,NULL,NULL,NULL),(2,'nisanth ','nis@gmail.com','$2b$10$yW8GhdseSqErpOixNUZ5AuJaTlG6ldMCYpEMRK/VN2BrR5vt3ioXm','user','2025-06-21 17:32:48',NULL,NULL,NULL,NULL),(3,'Nisanth ','nish@gmail.com','$2b$10$7t.RnZKG8Br2HRJleQLLlek/KXTFwb2kw2UZ28TfHLRrYakFmEaGK','user','2025-06-21 17:56:40',NULL,NULL,NULL,NULL),(4,'Nisanth','babd@gmsail.com','$2b$10$QmS/FTVRV.4iajw9UcIf4u7F2MkrNlbTxW/xw8ese..J0OBSESW5O','user','2025-06-22 05:36:18',NULL,NULL,NULL,NULL),(5,'adhii ','aadfg@gmail.com','$2b$10$YFCsxwT9AnXLMocop8ouquuBnhCsMAVlMVkJjZrbn.0OSDw7D7FQe','user','2025-06-22 05:43:00',NULL,NULL,NULL,NULL),(6,'adhii ','adhi@gmail.com','$2b$10$5PQ65LUyM0VttOvi4MnX1OszBJiIs6x04IYgTBL77h9.vVPbCHphW','user','2025-06-22 05:46:06','djbjbskjcbjdb bskjbk sjbfjbs','695573','999999999',NULL),(9,'Nisanth','nisanthsm@icloud.com','$2b$10$awxMRQcBdcuUaTXLHNZx3usFdMZkFNs92wL0SESoRvM.pGZCJKuOC','user','2025-06-22 09:38:18','prsanth bhavan kavalotukonam','695573','9809126735',NULL),(10,'asddada','adhi@gmail.comm','$2b$10$bRvXbKcPdnstPDJtwOqPsOuIDnwrX3waPvtBa0biR/cZr3UdtWeH2','user','2025-06-23 08:10:34','prsanth bhavan kavalotukonam','695573','9809126735',NULL);
+INSERT INTO `users` VALUES (6,'adhii ','adhi@gmail.com','$2b$10$5PQ65LUyM0VttOvi4MnX1OszBJiIs6x04IYgTBL77h9.vVPbCHphW','user','2025-06-22 05:46:06','djbjbskjcbjdb bskjbk sjbfjbs','695573','999999999',NULL),(10,'asddada','adhi@gmail.comm','$2b$10$bRvXbKcPdnstPDJtwOqPsOuIDnwrX3waPvtBa0biR/cZr3UdtWeH2','user','2025-06-23 08:10:34','prsanth bhavan kavalotukonam','695573','9809126735',NULL),(11,'Admin','admin@example.com','$2b$10$siiXF547bfN8NKrSCGzKU.dCzA9iDS8GkpOBxbQPKK9z5qxw4Krm6','admin','2025-06-23 17:21:55',NULL,NULL,NULL,NULL),(12,'John Doe','john@gmail.com','$2b$10$V1emjYx/JoqB/FMQvXaTRO1hD0atyI48/s874I9mBd9ZMso/IDPC.','user','2025-06-24 14:32:57','djbjbskjcbjdb bskjbk sjbfjbs','695573','999999999',NULL),(13,'John Doe','john@gmail.comm','$2b$10$3QOAu8S5rFp2F0QWs/.BSuni3D2H3uCl6EIHOXDf7xJqCK/KfoKba','user','2025-06-25 06:02:02','djbjbskjcbjdb bskjbk sjbfjbs','695573','999999999',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -116,4 +144,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-23 20:49:36
+-- Dump completed on 2025-06-29 11:47:51
